8fe82d8f04b93475680f598fd0c13996e7491339
Falkko <Falkko@KOMPUTER>
2022-05-08 00:46:37 +0300

    Добавьте файлы проекта.

---
+++ sosi/Source.cpp
@@ -0,0 +1,123 @@
+#include <conio.h>
+#include <iostream>
+using std::endl;
+using std::cout; 
+
+using std::cin;
+int main() {
+	int a;
+	cout << "Enter ur mmr: ";
+	cin >> a;
+	if (a < 154) {
+		cout << "Your rang is: Herald 1" << endl;
+	}
+	else if (a > 153||a < 308) {
+		cout << "Your rang is: Herald 2" << endl;
+	}
+	else if (a > 307||a < 462) {
+		cout << "Your rang is: Herald 3" << endl;
+	}
+	else if (a > 461||a < 616) {
+		cout << "Your rang is: Herald 4" << endl;
+	}
+	else if (a > 615 || a < 770) {
+		cout << "Your rang is: Herald 5" << endl;
+	}
+	else if (a > 769 || a < 924) {
+		cout << "Your rang is: Guardian 1" << endl;
+	}
+	else if (a > 923 || a < 1078) {
+		cout << "Your rang is: Guardian 2" << endl;
+	}
+	else if (a > 1077 || a < 1232) {
+		cout << "Your rang is: Guardian 3" << endl;
+	}
+	else if (a > 1231 || a < 1386) {
+		cout << "Your rang is: Guardian 4" << endl;
+	}
+	else if (a > 1386 || a < 1540) {
+		cout << "Your rang is: Guardian 5" << endl;
+	}
+	else if (a > 1539 || a < 1694) {
+		cout << "Your rang is: Crysader 1" << endl;
+	}
+	else if (a > 1693 || a < 1848) {
+		cout << "Your rang is: Crysader 2" << endl;
+	}
+	else if (a > 1847 || a < 2002) {
+		cout << "Your rang is: Crysader 3" << endl;
+	}
+	else if (a > 2001 || a < 2156) {
+		cout << "Your rang is: Crysader 4" << endl;
+	}
+	else if (a > 2155 || a < 2310) {
+		cout << "Your rang is: Crysader 5" << endl;
+	}
+	else if (a > 2309 || a < 2464) {
+		cout << "Your rang is: Archon 1" << endl;
+	}
+	else if (a > 2463 || a < 2618) {
+		cout << "Your rang is: Archon 2" << endl;
+	}
+	else if (a > 2617 || a < 2772) {
+		cout << "Your rang is: Archon 3" << endl;
+	}
+	else if (a > 2771 || a < 2926) {
+		cout << "Your rang is: Archon 4" << endl;
+	}
+	else if (a > 2925 || a < 3080) {
+		cout << "Your rang is: Archon 5" << endl;
+	}
+	else if (a > 3079 || a < 3234) {
+		cout << "Your rang is: Legend 1" << endl;
+	}
+	else if (a > 3233 || a < 3388) {
+		cout << "Your rang is: Legend 2" << endl;
+	}
+	else if (a > 3387 || a < 3542) {
+		cout << "Your rang is: Legend 3" << endl;
+	}
+	else if (a > 3541 || a < 3696) {
+		cout << "Your rang is: Legend 4" << endl;
+	}
+	else if (a > 3695 || a < 3850) {
+		cout << "Your rang is: Legend 5" << endl;
+	}
+	else if (a > 3849 || a < 4004) {
+		cout << "Your rang is: Ancient 1" << endl;
+	}
+	else if (a > 4003 || a < 4158) {
+		cout << "Your rang is: Ancient 2" << endl;
+	}
+	else if (a > 4157 || a < 4312) {
+		cout << "Your rang is: Ancient 3" << endl;
+	}
+	else if (a > 4311 || a < 4465) {
+		cout << "Your rang is: Ancient 4" << endl;
+	}
+	else if (a > 4464 || a < 4620) {
+		cout << "Your rang is: Ancient 5" << endl;
+	}
+	else if (a > 4619 || a < 4820) {
+		cout << "Your rang is: Divine 1" << endl;
+	}
+	else if (a > 4819 || a < 5020) {
+		cout << "Your rang is: Divine 2" << endl;
+	}
+	else if (a > 5019 || a < 5220) {
+		cout << "Your rang is: Divine 3" << endl;
+	}
+	else if (a > 5219 || a < 5420) {
+		cout << "Your rang is: Divine 4" << endl;
+	}
+	else if (a > 5419) {
+		cout << "Your rang is: Divine 5" << endl;
+	}
+
+
+
+
+	return 0;
+	_getch();
+
+}
